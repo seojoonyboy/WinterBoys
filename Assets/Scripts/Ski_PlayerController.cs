efeb7fb4d1dcd8a087412de569948ea78b3f64ed
seojoonwon <seojoonwon@naver.com>
2017-10-10 15:33:10 +0900

    플레이어가 커브 관련 코드 수정. 사용하지 않는 Physics Material 제거.

--- Assets/Scripts/Ski_PlayerController.cs
+++ Assets/Scripts/Ski_PlayerController.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Ski_PlayerController : MonoBehaviour {
-    public float speedForce = 0;
+    public float speedForce = 1.0f;
     public float torqueForce = 1.0f;
-    public float driftFactorSticky = 0.9f;
-    public float driftFactorSlippy = 1f;
+    public float driftFactor = 1.0f;
     public float maxStickyVelocity = 2.5f;
     public float minStickyVelocity = 1.5f;
     public float coneringLossVelocity = 1.0f;
@@ -15,17 +14,13 @@ public class Ski_PlayerController : MonoBehaviour {
     private void FixedUpdate() {
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
 
-        float driftFactor = driftFactorSticky;
-        //Debug.Log(RightVelocity().magnitude);
-        if (RightVelocity().magnitude > maxStickyVelocity) {
-            driftFactor = driftFactorSlippy;
-        }
-        rb.velocity = ForwardVelocity() + RightVelocity() * driftFactor;
+        //rb.velocity = ForwardVelocity() + RightVelocity() * driftFactorSlippy;
+        rb.AddForce(__ForwardVelocity());
 
-        //회전속도
         rb.angularVelocity = Input.GetAxis("Horizontal") * torqueForce;
-        Debug.Log(transform.eulerAngles.z);
-        //최대회전 각도 지정 (우측)
+
+        checkPlayerPos();
+
         if (transform.eulerAngles.z > 260) {
             if (rb.angularVelocity == 0) {
                 rb.constraints = RigidbodyConstraints2D.None;
@@ -45,29 +40,22 @@ public class Ski_PlayerController : MonoBehaviour {
             }
         }
 
-        //항상 Player 전방으로 Force 부여
-        //rb.AddForce(transform.up * speedForce);
-
-        //커브한 정도에 따라 감속
-        //...
-        //...
-        //Debug.Log(Vector3.Angle(transform.up, -Vector3.up));
         float angle = Vector3.Angle(transform.up, -Vector3.up);
+
         if (angle >= angleV) {
             //Debug.Log(transform.up.x);
             Vector3 val;
             if (transform.up.x < 0) {
                 val = new Vector3(-1, 0.6f, 0) * (angle / 150.0f);
-            }
-            else {
+            } else {
                 val = new Vector3(1, 0.6f, 0) * (angle / 150.0f);
             }
             rb.AddForce(val);
         }
+        //rb.AddForce(ForwardVelocity() * 2f);
         else {
-            rb.AddForce(__ForwardVelocity() * 2f);
+            rb.velocity = ForwardVelocity() + RightVelocity() * driftFactor;
         }
-        checkPlayerPos();
     }
 
     //전방으로 얼마나 추가적으로 힘을 가할지 (현재 속도 기준)
@@ -76,7 +64,7 @@ public class Ski_PlayerController : MonoBehaviour {
     }
 
     Vector3 __ForwardVelocity() {
-        return transform.up * Vector3.Dot(-Vector3.up, transform.up * 0.7f);
+        return transform.up * Vector3.Dot(-Vector3.up, transform.up) * speedForce;
     }
     
     //코너링시 코너링 방향으로 밀리는 힘의 크기 (현재 속도 기준)
