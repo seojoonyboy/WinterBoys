24e4a574b1adef64787e6390027342266e4578d4
이종욱 <wjlee729@fbl.kr>
2017-10-17 18:11:12 +0900

    스켈레톤 방향 잡기

--- Assets/Scripts/ViewController/Skeleton_PlayerController.cs
+++ Assets/Scripts/ViewController/Skeleton_PlayerController.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Skeleton_PlayerController : MonoBehaviour {
-	public Skeleton_Gyroscope gyro;
+	[SerializeField] Skeleton_Gyroscope gyro;
+	[SerializeField] private Transform background;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.localRotation = Quaternion.Euler(gyro.gyroscope_rotation);
+		GetComponent<Rigidbody2D>().AddForce(transform.right * -gyro.gyroscope_rotation.z);
+		var dir = background.transform.position - transform.position;
+		var angle = Mathf.Atan2(-dir.x, dir.y) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 }
