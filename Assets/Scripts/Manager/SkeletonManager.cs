95e3e655f04a52861d5e3750e531da2c7ee88277
이종욱 <wjlee729@fbl.kr>
2017-10-19 15:29:27 +0900

    스탄 에셋 아이폰 지원 취소(현재 에러남), 스켈레톤 씬 돌아가기 버튼 추가

---
+++ Assets/Scripts/Manager/SkeletonManager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SkeletonManager : MonoBehaviour {
+    public void mainLoad() {
+        SceneManager.LoadScene("Main");
+    }
+}
