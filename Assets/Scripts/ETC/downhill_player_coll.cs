57270c46dec24a3295f27975b7c194ee9ca042b7
seojoonwon <seojoonwon@naver.com>
2017-10-24 17:45:00 +0900

    폴 디자인 교체. (Spine Anim 추가) 플레이어와 부딪히는 경우 좌측 우측 날라가는 Anim 전환. Spine 관련 Resource 폴더 정리

--- Assets/Scripts/ETC/downhill_player_coll.cs
+++ Assets/Scripts/ETC/downhill_player_coll.cs
@@ -1,10 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using Spine.Unity;
 public class downhill_player_coll : MonoBehaviour {
     private DownhillManager dM;
     private void Awake() {
         dM = GameObject.Find("Manager").GetComponent<DownhillManager>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag == "Flag") {
+            var flagComp = other.GetComponent<FlagController>();
+            var anim = other.GetComponent<SkeletonAnimation>();
+            anim.loop = false;
+
+            if (flagComp.rayDir == FlagController.type.LEFT) {
+                anim.AnimationName = "broken_left";
+            }
+            else if(flagComp.rayDir == FlagController.type.RIGHT) {
+                anim.AnimationName = "broken_right";
+            }
+        }
     }
 }
