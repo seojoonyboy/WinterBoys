29ff4757fb2c0b0c139f8229f322822574093d0b
seojoonwon <seojoonwon@naver.com>
2017-11-09 15:59:26 +0900

    스키점프 초기 무작위 산악지형, 구름 생성 추가.

--- Assets/Scripts/Manager/SkiJumpBoardHolder.cs
+++ Assets/Scripts/Manager/SkiJumpBoardHolder.cs
@@ -22,7 +22,83 @@ public class SkiJumpBoardHolder : MonoBehaviour {
         offSetOfMountain,
         offSetOfCloud;
 
+    private int mountainIndex = 0;
+    private int cloudIndex = 0;
     private void Awake() {
         
+    }
+
+    private void Start() {
+        init();
+    }
+
+    private void init() {
+        mountainIndex = 0;
+        cloudIndex = 0;
+
+        Generate(bundleOfMountains, 0);
+        Generate(bundleOfClouds, 1);
+    }
+
+    private Vector2 randPoses(Vector2 origin, int type) {
+        Vector2 nextPos;
+        float 
+            xPos = 0,
+            yPos = 0;
+        switch (type) {
+            case 0:
+                xPos = origin.x + randNum(offSetOfMountain[0], offSetOfMountain[1]);
+                yPos = origin.y + randNum(offSetOfMountain[2], offSetOfMountain[3]);
+                break;
+            case 1:
+                xPos = origin.x + randNum(offSetOfCloud[0], offSetOfCloud[1]);
+                yPos = origin.y + randNum(offSetOfCloud[2], offSetOfCloud[3]);
+                break;
+        }
+        nextPos = new Vector2(xPos, yPos);
+        return nextPos;
+    }
+
+    private float randNum(int min, int max) {
+        int[] marks = { -1, 1 };
+        int mark = marks.Random();
+        float rand = Random.Range(min, max);
+        return rand * mark;
+    }
+
+    private void Generate(int num, int type) {
+        switch (type) {
+            //산악지형
+            case 0:
+                int mntRndIndex = Random.Range(0, mountainPrefs.Length - 1);
+                GameObject originObj = Instantiate(mountainPrefs[mntRndIndex]);
+                originObj.transform.position = new Vector2(intervalsOfMountains * mountainIndex, 0);
+                float totalMountainNum = Random.Range(1, num);
+                if (totalMountainNum > 1) {
+                    for (int i = 0; i < num - 1; i++) {
+                        mntRndIndex = Random.Range(0, mountainPrefs.Length);
+                        Debug.Log(mntRndIndex);
+                        GameObject obj = Instantiate(mountainPrefs[mntRndIndex]);
+                        obj.transform.position = randPoses(originObj.transform.position, 0);
+                    }
+                }
+                break;
+            case 1:
+            //구름
+                int cloudRndIndex = Random.Range(0, cloudPrefs.Length - 1);
+                GameObject cloudObj = Instantiate(cloudPrefs[cloudRndIndex]);
+                cloudObj.transform.position = new Vector2(intervalOfClouds * cloudIndex, 150);
+                float totalCloudNum = Random.Range(1, num);
+                if(totalCloudNum > 1) {
+                    for (int i = 0; i < num - 1; i++) {
+                        cloudRndIndex = Random.Range(0, cloudPrefs.Length);
+                        GameObject _obj = Instantiate(cloudPrefs[cloudRndIndex]);
+                        _obj.transform.position = randPoses(cloudObj.transform.position, 1);
+                    }
+                }
+                break;
+        }
+        cloudIndex++;
+        mountainIndex++;
     }
 }
