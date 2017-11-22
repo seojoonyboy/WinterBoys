d38909f0ac504705a8343da230b04e9ab4356181
seojoonwon <seojoonwon@naver.com>
2017-11-22 19:28:35 +0900

    스키점프 카메라 최상단 최하단 고정 영역 지정

--- Assets/Scripts/ETC/FixPosition.cs
+++ Assets/Scripts/ETC/FixPosition.cs
@@ -7,15 +7,29 @@ public class FixPosition : MonoBehaviour {
     public enum type { NULL, SKIJUMP_MAINCAM, SKIJUMP_CAM_TARGET }
     public Transform target;
 
+    private float camSize;
+    private void Awake() {
+    }
     private void Update() {
         if(_type == type.SKIJUMP_MAINCAM) {
-            //카메라의 하단이 바닥 영역을 넘어가거나
-
+            Vector2 camPos = transform.position;
+            Vector2 targetPos = target.position;
+            camSize = GetComponent<Camera>().orthographicSize;
             //카메라의 상단이 하늘 영역을 벗어나지 않도록 고정
+            GetComponent<Camera>().orthographicSize = 10f;
+            if (targetPos.y > 23) {
+                transform.position = new Vector3(target.position.x, transform.position.y, -10f);
+            }
+
+            //카메라의 하단이 바닥 영역을 벗어나지 않도록 고정
+            else if (targetPos.y < 10) {
+                transform.position = new Vector3(target.position.x, camSize, -10f);
+            }
 
             //그밖의 경우
-            GetComponent<Camera>().orthographicSize = 12f;
-            transform.position = target.position;
+            else {
+                transform.position = new Vector3(targetPos.x, targetPos.y, -10);
+            }
         }
         else if(_type == type.SKIJUMP_CAM_TARGET) {
             transform.position = new Vector3(target.position.x + 3f, target.position.y + 1f, -10f);
