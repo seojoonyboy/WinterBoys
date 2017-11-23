e2f10ab3f59db7d3182e82887acb582ac71a8180
seojoonwon <seojoonwon@naver.com>
2017-11-23 15:13:24 +0900

    스키점프 아이템이 현재 카메라 영역에 종속되어 Map에 생성되도록 추가. 구름 동적 제거. 아이템을 위한 태그 추가

---
+++ Assets/Scripts/ETC/ItemGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour {
+    public SportType gameType;
+    public GameObject[] items;
+    public Transform parent;
+
+    private float interval;
+    private Camera cam;
+
+    private void Start() {
+        if(gameType == SportType.SKIJUMP) {
+            interval = 300;
+            cam = Camera.main;
+        }
+    }
+
+    private void Update() {
+        if(gameType == SportType.SKIJUMP) {
+            Vector2 camPos = cam.transform.position;
+            if (camPos.x > interval) {
+                Generate(SportType.SKIJUMP);
+                interval *= 2;
+            }
+        }
+    }
+
+    public void Generate(SportType type) {
+        switch (type) {
+            case SportType.SKIJUMP:
+                Vector3 itemPos = randPos(SportType.SKIJUMP);
+                int randIndex = Random.Range(0, items.Length);
+                GameObject item = Instantiate(items[randIndex]);
+                item.transform.position = itemPos;
+                item.transform.SetParent(parent);
+                break;
+        }
+    }
+
+    private Vector3 randPos(SportType type) {
+        Vector3 pos = Vector3.zero;
+        switch (type) {
+            case SportType.SKIJUMP:
+                float randX = Random.Range(0, Screen.width);
+                float randY = Random.Range(0, Screen.height);
+
+                pos = cam.ScreenToWorldPoint(new Vector3(randX + Screen.width, randY, 0));
+                pos.z = 0;
+                break;
+        }
+        return pos;
+    }
+}
