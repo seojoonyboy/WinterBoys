09c5f1c162e769784dbfe20fac99a7ec8f9c9108
이종욱 <wjlee729@fbl.kr>
2017-12-15 16:11:11 +0900

    결과 화면 UI수정, 로고 추가, 앱 아이콘 추가

---
+++ Assets/FblLogo/Script/Logo.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Logo : MonoBehaviour {
+    [SerializeField]
+    private string sceneName;
+
+    public void nextScene() {
+        SceneManager.LoadScene(sceneName);
+    }
+}
