c963d7543c495efe34d8883c764a596329eac81f
seojoonwon <seojoonwon@naver.com>
2018-01-10 15:23:25 +0900

    튜토리얼 씬 추가. 튜토리얼 씬 내부에서 제어를 위한 Controller 스크립트 추가

---
+++ Assets/Scripts/ViewController/TutorialController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+    public Sprite[] backgrounds;
+    public Image background;
+    public GameObject[] dh_paragraph;
+    public GameObject[] sj_paragraph;
+
+    public void setBackgroundImage(int index, bool isLandscape) {
+        if (isLandscape) {
+
+        }
+        else {
+
+        }
+        background.sprite = backgrounds[index];
+    }
+
+    public void nextPage() {
+
+    }
+}
