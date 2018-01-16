9e58d69d4cd9b23761015fc041c97227d4d7fd17
seojoonwon <seojoonwon@naver.com>
2018-01-16 19:54:02 +0900

    스켈레톤 애니메이션 테스트용 스크립트 추가

---
+++ Assets/Scripts/ETC/reloadTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine.Unity;
+
+public class reloadTest : MonoBehaviour {
+    SkeletonAnimation anim;
+    private void Start() {
+        anim = GetComponent<SkeletonAnimation>();
+    }
+
+    public void FlipX() {
+        anim.initialFlipX = true;
+        anim.Initialize(true);
+    }
+}
