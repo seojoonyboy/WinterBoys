6a7abc04a94ef2bebf6b966998f8f648b5c171da
이종욱 <wjlee729@fbl.kr>
2018-01-23 17:38:08 +0900

    옵션 이미지 적용, 스키점프시 0초 미표기 오류 수정

--- Assets/Scripts/ViewController/Main/OptionController.cs
+++ Assets/Scripts/ViewController/Main/OptionController.cs
@@ -33,16 +33,12 @@ public class OptionController : MonoBehaviour {
 		efxToggle.isOn = gm.optionData.efx;
 		vibrateToggle.isOn = gm.optionData.vibrate;
 		bgmToggle.image.sprite = gm.optionData.bgm ? on : off;
-		bgmToggle.GetComponentInChildren<Text>().text = gm.optionData.bgm ? "ON" : "OFF";
 		efxToggle.image.sprite = gm.optionData.efx ? on : off;
-		efxToggle.GetComponentInChildren<Text>().text = gm.optionData.efx ? "ON" : "OFF";
 		vibrateToggle.image.sprite = gm.optionData.vibrate ? on : off;
-		vibrateToggle.GetComponentInChildren<Text>().text = gm.optionData.vibrate ? "ON" : "OFF";
 	}
 
 	private void setBgmToggle(bool value) {
 		bgmToggle.image.sprite = value ? on : off;
-		bgmToggle.GetComponentInChildren<Text>().text = value ? "ON" : "OFF";
 		gm.optionData.bgm = value;
 		gm.optionSave();
 		sm.Play(SoundManager.SoundType.EFX, "gameSelBtn");
@@ -51,7 +47,6 @@ public class OptionController : MonoBehaviour {
 
 	private void setEfxToggle(bool value) {
 		efxToggle.image.sprite = value ? on : off;
-		efxToggle.GetComponentInChildren<Text>().text = value? "ON" : "OFF";
 		gm.optionData.efx = value;
 		gm.optionSave();
 		sm.Play(SoundManager.SoundType.EFX, "gameSelBtn");
@@ -60,7 +55,6 @@ public class OptionController : MonoBehaviour {
 
 	private void setVibrateToggle(bool value) {
 		vibrateToggle.image.sprite = value ? on : off;
-		vibrateToggle.GetComponentInChildren<Text>().text = value ? "ON" : "OFF";
 		gm.optionData.vibrate = value;
 		gm.optionSave();
 		sm.Play(SoundManager.SoundType.EFX, "gameSelBtn");
