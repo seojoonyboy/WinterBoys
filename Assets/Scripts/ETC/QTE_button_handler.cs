81407ab28b0bc3bdba6e8cf86b8d75deb56344f5
seojoonwon <seojoonwon@naver.com>
2018-01-23 14:24:42 +0900

    QTE 디자인 변경. 성공 실패 결과 Text 추가.(버그 수정중)

--- Assets/Scripts/ETC/QTE_button_handler.cs
+++ Assets/Scripts/ETC/QTE_button_handler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using GameEvents;
 using UnityEngine.UI;
+using Spine.Unity;
 
 public class QTE_button_handler : MonoBehaviour {
     public SkiJumpManager sm;
@@ -14,6 +15,11 @@ public class QTE_button_handler : MonoBehaviour {
     private int qteCnt = 0;
     private int successCnt = 0;
     private bool isSuccess = false;
+
+    public GameObject 
+        successObj,
+        failObj;
+
     private void Awake() {
         animator = GetComponent<Animator>();
 
@@ -22,13 +28,39 @@ public class QTE_button_handler : MonoBehaviour {
     }
 
     private void OnEnable() {
-        qteCnt = 0;
         nextQTE();
     }
 
     public void OnClick() {
+        successObj.SetActive(false);
+        failObj.SetActive(false);
+
+        successObj.GetComponent<SkeletonGraphic>().Skeleton.SetToSetupPose();
+        failObj.GetComponent<SkeletonGraphic>().Skeleton.SetToSetupPose();
+
+        AnimatorStateInfo animState = animator.GetCurrentAnimatorStateInfo(0);
+        AnimatorClipInfo[] myAnimatorClip = animator.GetCurrentAnimatorClipInfo(0);
+        float stopedAnimTime = myAnimatorClip[0].clip.length * animState.normalizedTime;
+        animator.speed = 0.0f;
+
+        if (stopedAnimTime > 0.99f) {
+            isSuccess = false;
+        }
+        else if (stopedAnimTime < 0.99f && stopedAnimTime > 0.45f) {
+            isSuccess = true;
+        }
+        else {
+            isSuccess = false;
+        }
+
+        Vector3 newPosOfResultText = new Vector3(rect.localPosition.x, rect.localPosition.y + 10f, 0);
         if (isSuccess) {
-            successCnt++;
+            successObj.GetComponent<RectTransform>().localPosition = newPosOfResultText;
+            successObj.SetActive(true);
+        }
+        else {
+            failObj.GetComponent<RectTransform>().localPosition = newPosOfResultText;
+            failObj.SetActive(true);
         }
         nextQTE();
     }
@@ -36,31 +68,23 @@ public class QTE_button_handler : MonoBehaviour {
     private void nextQTE() {
         qteCnt++;
 
-        float randX = Random.Range(-780, 780);
-        float randY = Random.Range(-330, 330);
+        float randX = Random.Range(-630, 630);
+        float randY = Random.Range(-300, 300);
 
         rect.localPosition = new Vector3(randX, randY, 0);
 
         if(qteCnt <= 3) {
-            animator.enabled = true;
             animator.updateMode = AnimatorUpdateMode.UnscaledTime;
             animator.Play("QTE", -1, 0);
+            animator.speed = 1.0f;
 
             sm.qte_magnification = successCnt * 0.1f;
-            Debug.Log(sm.qte_magnification);
         }
         else {
             _eventManager.TriggerEvent(new SkiJump_QTE_end());
-            Debug.Log("QTE 종료 트리거");
-        }
-    }
+            rect.gameObject.SetActive(false);
 
-    public void SetState(int value) {
-        if(value == 1) {
-            isSuccess = true;
-        }
-        else {
-            isSuccess = false;
+            qteCnt = 0;
         }
     }
 }
